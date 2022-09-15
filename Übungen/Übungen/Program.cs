5d54537f7af87a28d2a79717682cd6dd2402b90a
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-15 14:58:34 +0200

    Lösung For-Schleife Aufgabe 2 #12

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -13,17 +13,13 @@ namespace Übungen
         {
             Console.WriteLine("Nenne mir eine Zahl bis maximal 20");
             int Zahl1 = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            int new_sum = 0;
-            for(int i = 0; i < Zahl1; i++)
+            for (int i = Zahl1; i >=0; i--)
             {
-                new_sum = sum + i ; 
-                Console.WriteLine(sum);
-                Console.WriteLine(sum + "+" + i + " = " + new_sum);
-                sum = new_sum;
+                Console.WriteLine(i);
             }
+            Console.ReadKey();
 
-            Console.ReadLine();
+           
         }
     }
 }
