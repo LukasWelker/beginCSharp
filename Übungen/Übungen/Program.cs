fedf62c3ca7136afe2ac95bc4db8b6bb3c8a5e19
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-15 11:33:55 +0200

    Int Projekt

---
+++ Übungen/Übungen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Übungen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zahl1 = 2;
+            int Zahl2 = 3;
+            Console.WriteLine(Zahl1 + Zahl2);
+            Console.ReadLine();
+
+        }
+    }
+}
