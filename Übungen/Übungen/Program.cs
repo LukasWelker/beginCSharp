5ccfd3872e45fe31320c28e72edb2b69d97ab3f0
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-16 15:56:09 +0200

    Fortschritt im Glücksspiel mit Zufallsvariable k

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -11,68 +11,69 @@ namespace Übungen
     {
         static void Main(string[] args)
         {
-            //Aufgabe 1
-            List<int> numbers = new List<int>() { 5, 3, 7 };
-            
-            //Aufgabe2
-                numbers.Add(1);
-          
-            //Aufgabe 3
-            while (true) 
-            { 
-
-                Console.WriteLine("Gib mir eine Zahl");
-                string eingabe2 = Console.ReadLine();
-                if (eingabe2 == string.Empty)
+            //ganzzahlige Zufallvariable k im Intervall I=[0;...;36]
+            Random r = new Random();
+            int budget = 10000;
+            int einsatz = 10;
+            bool lastgame_won = true;
+            bool nullgeworfen = false;
+            // ZU beginn ist der Countdown 0
+            int count = 0;
+            //Läuft die Schleife die ganze Zeit durch wenn mehr als 0 Euro vorhanden sind
+            while (budget > 0)
+            {
+                //Wenn man mindestens 10k hat und das letzte Spiel gewonnen hat setzt man 10 Euro
+                if (budget >= 10000 && lastgame_won == true && nullgeworfen == false)
                 {
-                    break;
+                    einsatz = 10;
                 }
-                int eingabe = Convert.ToInt32(eingabe2);
+                //Wenn man mindestens 10k besitzt und das Letzte Spiel verloren hat verdoppelt sich der einsatz
+                else if (budget >= 10000 && lastgame_won == false && nullgeworfen == false)
+                {
+                    einsatz = 20;
+                }
+                //Wenn man weniger als 10k besitzt verdoppelt sich der gesamtverlust dieser wird der einsatz
+                else if (budget < 10000 && nullgeworfen == false)
+                {
+                    int gesamtverlust = 10000 - budget;
+                    einsatz = 2 * gesamtverlust;
+                }
+                //Einsatz darf maximal 1000Euro sein
+                einsatz = Math.Min(1000, einsatz);
+                //Einsatz ist maximal die Höhe meines Budget
+                einsatz = Math.Min(einsatz, budget);
 
-                numbers.Add(eingabe);
+                int random_zahl = r.Next(0, 36);
+                budget = budget - einsatz;
+                nullgeworfen = false;
+                //Wenn k gerade ist und k ungleich 0 ist erhalte ich 20 Euro
+                if (random_zahl % 2 == 0 && random_zahl != 0) 
+                {
+                    int gewinn = 2 * einsatz;
+                    budget = budget + gewinn;
+                    lastgame_won = true;
+                }
+                // Wenn k gleich 0 ist wird der einsatz abgezogen aber der einsatz bleibt gleich
+                else if (random_zahl == 0)
+                {
+                   nullgeworfen = true;
+                    lastgame_won = false;   
+                }
+                //Wenn k ungerade ist verlieren sie ihren einsatz aber am einsatz verändert sich nichts
+                else if  (random_zahl % 2 == 1)
+                {
+                    lastgame_won = false;
+                }
+                // Zählt den Coun´t jedes mal wenn die Schleife einmal durchgelaufen ist addiert immer plus 1 wegen dem ++
+                count ++;
             }
 
-            //Aufgabe 4
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                //Eckige Klamme, um den Inhalt meiner Liste zu zeigen
-                Console.WriteLine(numbers[i]);
-            }
-                
-            //Aufgabe 5
-            numbers.Insert(1, 99);
 
-            //Aufgabe 6
-            numbers.AddRange(new List<int>() { 92, 93, 94});
-
-
-            //Aufgabe7
-            numbers.RemoveAt(1);
-
-            //Aufgabe 8
-            numbers.Remove(99);
-
-            //Aufgabe 9
-            numbers.RemoveAt(numbers.Count - 1);
-
-            //Aufgabe 10
-
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                Console.WriteLine(numbers[i]);
-            }
-
-            //Aufgabe 11 ----> Lösche alle Zahlen größer als 80 aus der Liste
-            numbers.RemoveAll(x => x < 80);
-
-
-
+            Console.WriteLine(count);
             Console.ReadLine();
-
         }
         
     }
 
 }
 
-// While
