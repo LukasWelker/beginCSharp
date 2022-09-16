aad07bed4f7d63d3f5298888d9c02bca0eb31211
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-16 09:30:20 +0200

    Aufgabe 5 For_Schleife

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -12,42 +12,27 @@ namespace Übungen
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Wie viele Einträge möchtest du in deinem Array eintragen?");
-            int user_entries = Convert.ToInt32(Console.ReadLine());
-            int[] zahlen = new int[user_entries];
-
-            //Startvariable/Laufzeit/Zählvariable
-            for (int i = 0; i < user_entries; i++)
+            Console.WriteLine("Wir Spielen ein Fizz-Buzz-Spiel zwischen den Zahlen 1 bis 100!");
+            for (int i = 0; i < 101; i--)
             {
-                Console.WriteLine("Gebe mir die erste Zahl deines Arrays an!");
-                zahlen[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            for (int i = 0; i < zahlen.Length; i++)
-            {
-                Console.WriteLine(zahlen[i]);
-            }
-
-
-            //Variante 1: Läufst arraay durch, hast eine Hilfsvariable int max = 0; Bei jedem durchlauf wird max überprüftob größer als aktulle zahl
-            int max = 0;
-            int min = zahlen[0];
-            double sum = 0;
-            for(int i = 0; i< zahlen.Length; i++)
-            {
-                if(max < zahlen[i])
+                if (i % 3==0 && i % 5 == 0)
                 {
-                    max = zahlen[i];
+                    Console.WriteLine("FizzBuzz");
                 }
-                if (min > zahlen[i]) 
+                else if (i % 3 == 0)
                 {
-                    min = zahlen[i];
+                    Console.WriteLine("Fizz");
                 }
-                sum = sum + zahlen[i];
+                 else if (i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                { 
+                        
+                    Console.WriteLine(i);
+                }
             }
-            double avg = sum / zahlen.Length;
-            Console.WriteLine(avg);
-            Console.WriteLine(min);
-            Console.WriteLine(max);
+            
             Console.ReadLine();
         }
     }
