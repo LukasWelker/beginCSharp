51e3b49f41977134a6e95ce4f30efa82b66419f8
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-19 10:53:56 +0200

    Methoden und Klassen Autobeispiel

---
+++ Übungen/Übungen/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Übungen
+{
+    public class Car
+    {
+        //Attribute
+        public int TypeId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int CCM { get; set; }
+        public string Color { get; set; }
+        public string TypeName { get; set; }
+        public int Tyres { get; set; }
+
+
+        //Konstruktor besondere Art von Methode zum Erzeugen
+        public Car(int typeId, string make, string model, string typeName, int ccm, string color, int Tyre)
+        {
+            TypeId = typeId;
+            Make = make;
+            Model = model;
+            TypeName = typeName;
+            CCM = ccm;
+            Color = color;
+            Tyres = Tyre;
+
+        }
+        public Car()
+
+        {
+
+        }
+
+        //METHODEN
+        public static Car Ask_UserforCar()
+        {
+            Car car = new Car();
+            Console.WriteLine("Was für ein Auto möchtest du haben(IdNummer)");
+            car.TypeId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Was für eine Automarke möchten sie haben");
+            car.Make = Console.ReadLine();
+
+            Console.WriteLine("Was für ein konkretes Model möchten sie?");
+            car.Model = Console.ReadLine();
+
+            Console.WriteLine("Was genau ist der Typename");
+            car.TypeName = Console.ReadLine();
+
+            Console.WriteLine("Wie viel Hubraum soll das Auto haben?");
+            car.CCM = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Welche Farbe soll das Auto haben");
+            car.Color = Console.ReadLine();
+
+            Console.WriteLine("Wie viele Räder soll das Auto haben?");
+            car.Tyres = Convert.ToInt32(Console.ReadLine());
+            return car;
+        }
+        public void PrintCarInformation()
+        {
+            Console.WriteLine($"Mein Fahrzeug ist ein  {Make}  {Model} {TypeName} mit einem Hubraum von {CCM} in der Farbe {Color} mit {Tyres} Reifen");
+        }
+
+
+
+       
+    }
+}
--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -11,90 +11,33 @@ namespace Übungen
     {
         static void Main(string[] args)
         {
-            //ganzzahlige Zufallvariable k im Intervall I=[0;...;36]
-            Random k = new Random();
-            int random_zahl;
+            Console.WriteLine("How many cars would u like to add");
 
-
-
-            int budget = 10000;
-
-       
-            int einsatz = 10;
-            bool lastgame_won = true;
-           
-            // ZU beginn ist der Countdown 0
-            int count = 0;
-            //Läuft die Schleife die ganze Zeit durch wenn mehr als 0 Euro vorhanden sind
-            do
+            int amount = Convert.ToInt32(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < amount; i++)
             {
-                 random_zahl = k.Next(0, 36);
-                //Wenn man mindestens 10k hat und das letzte Spiel gewonnen hat setzt man 10 Euro
-                if (budget >= 10000 && lastgame_won == true )
-                {
-                    einsatz = 10;
-                }
-                //Wenn man mindestens 10k besitzt und das Letzte Spiel verloren hat verdoppelt sich der einsatz
-                else if (budget >= 10000 && lastgame_won == false)
-                {
-                    einsatz = 20;
-                }
-                //Wenn man weniger als 10k besitzt verdoppelt sich der gesamtverlust dieser wird der einsatz
-                else if (budget < 10000)
-                {
-                    int gesamtverlust = 10000 - budget;
-                    einsatz = 2 * gesamtverlust;
-
-                }
-                ////Einsatz darf maximal 1000Euro sein
-                einsatz = Math.Min(1000, einsatz);
-                ////Einsatz ist maximal die Höhe meines Budget
-                einsatz = Math.Min(einsatz, budget);
-
-
-                budget = budget - einsatz;
-               
-                //Wenn k gerade ist und k ungleich 0 ist erhalte ich 20 Euro
-                if (random_zahl % 2 == 0 && random_zahl != 0)
-                {
-                    int gewinn = 2 * einsatz;
-                    budget = budget + gewinn;
-                    lastgame_won = true;
-                    Console.WriteLine("You won!");
-                }
-                // Wenn k gleich 0 ist wird der einsatz abgezogen aber der einsatz bleibt gleich
-                else if (random_zahl == 0)
-                {
-                    Console.WriteLine("nothing happend");
-                    lastgame_won = false;
-                }
-                //Wenn k ungerade ist verlieren sie ihren einsatz aber am einsatz verändert sich nichts
-                else if (random_zahl % 2 == 1)
-                {
-                    budget = budget - einsatz;
-                    lastgame_won = false;
-                    Console.WriteLine("You lost!");
-                }
-                // Zählt den Coun´t jedes mal wenn die Schleife einmal durchgelaufen ist addiert immer plus 1 wegen dem ++
-                count++;
-                Console.WriteLine($"Dein aktuelles Budget beträgt: {budget}");
-                Console.WriteLine($"Dein aktueller Einsatz beträgt: {einsatz}");
-                Console.WriteLine($"Randomzahl: {random_zahl}");
-
-
-
-
+                
+                cars.Add(Car.Ask_UserforCar());
             }
-            while (budget > 0 ); 
-       
-            Console.WriteLine($"Das ist dein momentaner Speielcounter: {count}");
-            Console.WriteLine($"Das ist dein momentaner Kontostand: {budget}");
+           
+            
+            foreach (Car car in cars)
+            {
+                //
+                car.PrintCarInformation();
+            }
 
 
 
+            
+           
+            
             Console.ReadLine();
         }
+
         
+
     }
 
 }
