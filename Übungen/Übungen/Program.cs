9cb2d32a6b81be469c172aa1d42df05b57cd8029
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-20 10:50:25 +0200

    While Schleife Aufgabe Buchstaben im Namen zählen

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Übungen
@@ -11,25 +12,56 @@ namespace Übungen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-
-            List<Tierarten> tierarten = new List<Tierarten>();
-            tierarten.Add(new Penguin("Lilo", 12, 56, 18, 18, true));
-            tierarten.Add(new Shark("Nami", 78,200, 280, 237, false));
-            tierarten.Add(new owl("Lola", 6, 66, 54, 9, true));
-
-            foreach (Tierarten t in tierarten)
+            Console.WriteLine("Hallo lieber User wie ist dein Name?");
+            string name =  Console.ReadLine();
+            while (name == string.Empty)
             {
-                t.Trinken();
-                t.Essen();
-                t.Geräusche();
-                Console.WriteLine(t.ToString());
-                Console.WriteLine("");
+                Console.WriteLine("Warum ignoriest du mich wie ist dein Name?");
+                name = Console.ReadLine();
+               
             }
+            Console.WriteLine($"Nett dich kennenzulernen {name} !");
 
+            int i = 0;
+            int count = 0;
+            while (i < name.Count())
+            {
+                //Überprüfen ob ein a vorhanden ist
+                //Kann vielseiteig verwendet werden!
+                if (name[i] == 'a')
+                {
+                    Console.WriteLine("A ist vorhanden");
+                    count++;
+                }
+                i++;
+            }
+           
+            Console.WriteLine($"so oft ist der Buchstabe a in ihrem Namen vorhanden: {count} Mal !");
 
+            Console.WriteLine();
 
+            Console.WriteLine("Gebe mir nun noch bitte 2 Zahlen ein, erstmal nur die erste!");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vielen Dank für die erste Zahl gebe mir nun bitte noch die 2. Zahl ein");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Möchtest du die größere Zahl wissen (y/n)");
+            string answer = Console.ReadLine();
+            
+            while (answer == "y")
+                do
+                {
+                    Console.WriteLine(Math.Max(zahl1, zahl2));
+                    break;
+                } 
+                while (answer == "n");
+                {
+                Console.WriteLine($"Pech gehabt das ist deine größere Zahl {Math.Max(zahl1, zahl2)}");
+                }
+                    
+               
+                
+               
 
             Console.ReadLine();
 
