a87aab3f1d94af54ab87e34345927d946eda019c
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-21 10:53:16 +0200

    Tschenerechner v2.

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -17,25 +17,62 @@ namespace Übungen
     {
         static void Main(string[] args)
         {
-            int[] zahleneinträge = new int[] { 32, 12, 34, 45 };
-            int wahresergebnis = SummeAusEinträgen(zahleneinträge);
-            Console.WriteLine(wahresergebnis);
+            Console.WriteLine("Hallo, geb mir bitte deine erste Zahl an!");
+            float zahl1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Vielen Dank geb mir nun bitte deine zweite Zahl an!");
+            float zahl2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Jetzt fehlt nur noch der Rechenoperator!");
+            string operators = Console.ReadLine();
+            float result = 0;
+            switch (operators)
+            {
+                case "+":
+                    result = Add(zahl1, zahl2);
+                    break;
+                case "-":
+                    result = Subtrahieren(zahl1, zahl2);
+                    break;
+                case "/":
+                    result = Division(zahl1, zahl2);
+                    break;
+                case "*":
+                    result = Multiply(zahl1, zahl2);
+                    break;
+                default:
+                    Console.WriteLine("Parameter gibt es nicht!");
+                    break;
+
+
+            }
+            Console.WriteLine($"Das Ergebnis ist {result}");
             Console.ReadLine();
         }
 
-        static int SummeAusEinträgen(int[] zahleneinträge)
-        {
-            int ergebnis = 0;
-            foreach (int zahleneintrag in zahleneinträge)
-            {
-                ergebnis = zahleneintrag+ ergebnis;
-                
-            }
-            return ergebnis;
-        }
-    }
- 
 
-}
+
+            static float Add(float zahl1, float zahl2)
+            {
+                float ergebnis = zahl1 + zahl2;
+                return ergebnis;
+            }
+            static float Subtrahieren(float zahl1, float zahl2)
+            {
+                float ergebnis = zahl1 - zahl2;
+                return ergebnis;
+            }
+            static float Multiply(float zahl1, float zahl2)
+            {
+                float ergebnis = zahl1 * zahl2;
+                return ergebnis;
+            }
+            static float Division(float zahl1, float zahl2)
+            {
+                float ergebnis = zahl1 / zahl2;
+                return ergebnis;
+            }
+        }
+
+
+    }
 
 
