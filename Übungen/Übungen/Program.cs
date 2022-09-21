4c9bf1502246afde483383aeb4cc35e37859470f
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-21 09:30:06 +0200

    Methode strings zu addieren

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -16,21 +17,20 @@ namespace Übungen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gebe mir Zhal 1");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Gebe mir eine Zahl2");
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
-            int ergebnis = AddierteZahlen(zahl1, zahl2);
-            Console.WriteLine(ergebnis);
+            string zahltext1 = "34";
+            string zahltext2 = "45";
+            int ergebins = AddierteZahlen(zahltext1, zahltext2);
+            Console.WriteLine(ergebins);
             Console.ReadLine();
-            
         }
 
-        static int AddierteZahlen(int zahl1, int zahl2)
+        static int AddierteZahlen(string zahltext1, string zahltext2)
         {
+           int zahl1 = Convert.ToInt32(zahltext1);
+           int zahl2 = Convert.ToInt32(zahltext2);
             int sum = zahl1 + zahl2;
             return sum;
+            
         }
     }
    
