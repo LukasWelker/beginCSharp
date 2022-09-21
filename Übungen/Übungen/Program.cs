c4c72c6e0534c72a628b0d16be516850e5faa676
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-21 08:52:47 +0200

    Dasselbe wie zuvor nur mit Namen die mit N enden

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -17,7 +17,7 @@ namespace Übungen
         {
             Console.WriteLine();
             List<string> allTrainees = new List<string>() { "Lukas", "Philipp", "Mona" };
-            List<string> filteredTrainees = GiveMeNamesWithM(allTrainees);
+            List<string> filteredTrainees = TraineesEndWithN(allTrainees);
             foreach (string trainee in filteredTrainees)
             {
                 Console.WriteLine(trainee); 
@@ -25,21 +25,33 @@ namespace Übungen
             }
             Console.ReadLine();
         }
-        static List<string> GiveMeNamesWithM(List<string> allTrainees)
-        {
-            List<string> traineeswithm= new List<string>();
-            //Datentyp      Variablenname //in operator Ausgangsvariable
-            foreach (string trainee in allTrainees)
-            {
-                if (trainee.ToLower().StartsWith("m"))
-                {
-                    traineeswithm.Add(trainee);
+        //static List<string> GiveMeNamesWithM(List<string> allTrainees)
+        //{
+        //    List<string> traineeswithm= new List<string>();
+        //    //Datentyp      Variablenname //in operator Ausgangsvariable
+        //    foreach (string trainee in allTrainees)
+        //    {
+        //        if (trainee.ToLower().StartsWith("m"))
+        //        {
+        //            traineeswithm.Add(trainee);
 
-                }
+        //        }
                 
 
+        //    }
+        //    return traineeswithm;
+        //}
+        static List<string> TraineesEndWithN(List <string> allTrainees)
+        {
+        List<string> traineesendwithn = new List<string>();
+            foreach (string trainee in allTrainees)
+            {
+                if (trainee.EndsWith("N"))
+                {
+                    allTrainees.Add(trainee);
+                }
             }
-            return traineeswithm;
+        return traineesendwithn;
         }
 
     }
