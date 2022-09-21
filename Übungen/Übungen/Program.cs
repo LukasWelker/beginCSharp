d23d127aa3c28f1df602cfa738c4be431b305623
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-21 08:30:40 +0200

    Aufgabe eine Methode zu erstellen, in der alle Namen , die mit M beginnen zurückgegeben werden

--- Übungen/Übungen/Program.cs
+++ Übungen/Übungen/Program.cs
@@ -15,66 +15,35 @@ namespace Übungen
     {
         static void Main(string[] args)
         {
-            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
-
-            ////fi.CopyTo("C:/Projects001/beginCSharp/MeineDatei13");
-            ////FileInfo fi_neu = new FileInfo("C:/Projects001/beginCSharp/MeineDatei13");
-            ////fi_neu.Delete();
-            //Console.WriteLine("Gebe mir den Pfad an wohin die Datei vershcieben werden soll");
-            //string path =  Console.ReadLine();
-            //path = path.Replace("\\, "\\\\";
-            //DirectoryInfo di = new DirectoryInfo(path);
-            //while (di.Exists == false)
-            //{
-            //    Console.WriteLine("Dein Pfad existiert nicht gib mir einen neuen!");
-            //    path = Console.ReadLine();
-            //    di = new DirectoryInfo(path);
-            //}
-            //Console.WriteLine("Wie soll die Datei heißen?");
-            //string fileName = Console.ReadLine();
-
-            //fi.CopyTo(path +"\\"+ fileName);
-            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
-
-            //if (fi.Exists)
-            //{
-            //    //Execute only if File exists
-            //}
-
-            string path = @"C:\Projects001Batei123";
-            StreamReader sr = new StreamReader(path);
-            //legiert unseere bool Wert das !
-            List<string[]> einträge = new List<string[]>();
-            //Remove Header
-            sr.ReadLine();
-            while(!sr.EndOfStream)
+            Console.WriteLine();
+            List<string> allTrainees = new List<string>() { "Lukas", "Philipp", "Mona" };
+            List<string> filteredTrainees = GiveMeNamesWithM(allTrainees);
+            foreach (string trainee in filteredTrainees)
             {
-                einträge.Add(sr.ReadLine().Split(';'));
+                Console.WriteLine(trainee); 
+                Console.ReadLine();
             }
-            for (int alter = 18; alter < 101; alter++)
-            using (StreamWriter writer = new StreamWriter($@"C:\Projects001\beginCSharp\gewicht_{alter}.csv"))
+            Console.ReadLine();
+        }
+        static List<string> GiveMeNamesWithM(List<string> allTrainees)
+        {
+            List<string> traineeswithm= new List<string>();
+            //Datentyp      Variablenname //in operator Ausgangsvariable
+            foreach (string trainee in allTrainees)
             {
-                for (int i = 0; i < einträge.Count; i++)
+                if (trainee.ToLower().StartsWith("m"))
                 {
-                    if (Convert.ToInt32(einträge[i][2]) == alter )
-                    {
-                        //\t bedeutet Tabulator
-                        writer.WriteLine(string.Join(";", einträge[i]));
-                        Console.WriteLine(string.Join("\t", einträge[i]));
-                    }
-                    //string.Join gibt einen string zurück Liste aus Parametern kann man zsm kleben
+                    traineeswithm.Add(trainee);
 
                 }
+                
 
             }
-            
-            Console.ReadLine();
-
+            return traineeswithm;
         }
-
-        
 
     }
 
 }
+
 
